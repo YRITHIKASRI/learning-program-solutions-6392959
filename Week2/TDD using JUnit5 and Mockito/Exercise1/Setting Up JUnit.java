bf64250cd1ef80b12c38cdad3a6838442ade59b1
YRITHIKASRI <rithikayuvaraj1911@gmail.com>
2025-06-29 20:18:11 +0530

    .

--- Week2/TDD using JUnit5 and Mockito/Exercise1/Setting Up JUnit.java
+++ Week2/TDD using JUnit5 and Mockito/Exercise1/Setting Up JUnit.java
@@ -1,1 +1,22 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CalculatorTest {
+
+    @Test
+    void testAdd() {
+        Calculator calc = new Calculator();
+        int result = calc.add(2, 3);
+        assertEquals(5, result, "2 + 3 should be 5");
+    }
+
+    @Test
+    void testSubtract() {
+        Calculator calc = new Calculator();
+        int result = calc.subtract(5, 2);
+        assertEquals(3, result, "5 - 2 should be 3");
+    }
+}
 
